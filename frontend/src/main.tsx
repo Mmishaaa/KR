b8804888786071f38b19022466aed77f073469ff
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-11-17 23:47:55 +0300

    add base store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { store } from "../state/store"
 
 const theme = createTheme({
   typography: {
@@ -12,8 +14,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-        <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
