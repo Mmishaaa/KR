2349961275c9433b29144c5c22792e8cce5421a6
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-12-15 16:28:28 +0300

    add models

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mySequelize from "./src/db/db.js";
+import { models } from "./src/models/models.js"
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
-app.use(express.json());  // Для парсинга JSON в теле запроса
+app.use(express.json());
+
+const start = async () => {
+  try{  
+    await mySequelize.authenticate()
+    await mySequelize.sync()
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}
 
 // Пример данных о пользователях
 let users = [
@@ -122,6 +139,4 @@ app.delete('/api/users/:id', (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+start();
